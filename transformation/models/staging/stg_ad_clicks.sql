6b397cad188585925987ff29eb431906b9c5117f
RedKnightM <rkatkori@ucsc.edu>
2026-01-20 18:47:55 -0800

    Staged all four raw tables

---
+++ transformation/models/staging/stg_ad_clicks.sql
@@ -0,0 +1,24 @@
+with source as (
+    select * from {{ source('ecommerce', 'raw_ad_click') }}
+),
+
+renamed as (
+    select
+        event_id,
+        user_id,
+        -- Timestamps always need casting from String to Timestamp
+        TIMESTAMP(timestamp) as occurred_at,
+        DATE(TIMESTAMP(timestamp)) as date_key,
+        
+        -- Device & Location (Standard fields)
+        device,
+        location,
+        
+        -- Ad Specifics (Handle NULLs if necessary)
+        coalesce(ad_source, 'unknown') as ad_source,
+        coalesce(campaign_id, 'organic') as campaign_id
+
+    from source
+)
+
+select * from renamed
---
+++ transformation/models/staging/stg_page_views.sql
@@ -0,0 +1,22 @@
+with source as (
+    select * from {{ source('ecommerce', 'raw_page_view') }}
+),
+
+renamed as (
+    select
+        event_id,
+        user_id,
+        product_id,
+        product_name,
+        category,
+        price,
+        location,
+        device,
+        -- Parse the string timestamp into a real BigQuery TIMESTAMP
+        TIMESTAMP(timestamp) as occurred_at,
+        -- Extract the date for partitioning
+        DATE(TIMESTAMP(timestamp)) as date_key
+    from source
+)
+
+select * from renamed
